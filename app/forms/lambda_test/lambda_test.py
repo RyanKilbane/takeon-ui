3bb3d32a61d00926451405615bceb5293d0cfe1d
RyanKilbane <RyanKilbane92@hotmail.com>
2019-06-21 12:33:48 +0100

    removed unused inports

--- app/forms/lambda_test/lambda_test.py
+++ app/forms/lambda_test/lambda_test.py
@@ -1,8 +1,5 @@
 import json
-from flask import Blueprint, request, render_template, redirect, url_for
-from app.setup import discovery_service
-from app.utilities.helpers import decompose_data, build_uri, build_json, get_user, forms_connect_to_eureka, \
-                                  forms_connect_to_eureka_validation, build_uri_2
+from flask import Blueprint
 import boto3
 
 lambda_test_blueprint = Blueprint(name='lambda_test',
