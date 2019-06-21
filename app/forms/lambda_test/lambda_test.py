3d6ff3e24c1dd640be795956008f56aa89825caa
RyanKilbane <RyanKilbane92@hotmail.com>
2019-06-21 12:32:43 +0100

    found an error in the dict

--- app/forms/lambda_test/lambda_test.py
+++ app/forms/lambda_test/lambda_test.py
@@ -12,5 +12,6 @@ lambda_test_blueprint = Blueprint(name='lambda_test',
 @lambda_test_blueprint.route("/lambda_test/<name>")
 def lambda_test(name):
     client = boto3.client("lambda")
-    response = client.invoke(FunctionName="takeon-lambda-test", Payload=json.dumps(name).encode("utf-8"))
+    data = {"name": name}
+    response = client.invoke(FunctionName="takeon-lambda-test", Payload=json.dumps(data).encode("utf-8"))
     return response
