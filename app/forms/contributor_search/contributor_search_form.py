9e379302edb9b5457762c734cf7a59321041c73c
OliverHarris <githarris@harriso.co.uk>
2019-08-14 14:59:17 +0100

    Removed mocking from file

--- app/forms/contributor_search/contributor_search_form.py
+++ app/forms/contributor_search/contributor_search_form.py
@@ -1,9 +1,5 @@
 import json
 
-#Mocking
-import app.settings
-import hashlib
-
 from urllib.error import URLError
 #from content_management import Content
 from flask import request, Blueprint, redirect, url_for, render_template
@@ -230,18 +226,8 @@ def general_search_screen():
         # Make a get request from the built up URL, when the request
         # is made, the url is passed over to the Persistence layer
 
-        if app.settings.MOCKING is False:
-            data = discovery_service.contributor_search(url_connect, "persistence-layer")
-        else:
-            string_value = str(request.form)
-            hash_value = hashlib.md5(string_value.encode()).hexdigest()
-            try:
-                with open('app/mock_suite/dummy_data/BDD/'+str(hash_value)+".json", 'r') as myfile:
-                    data = myfile.read()
-            except FileNotFoundError:
-                print("Mocking file was not found. Looking for "+str(hash_value)+".json")
-                with open('app/mock_suite/dummy_data/BDD/failed.json', 'r') as myfile:
-                    data = myfile.read()
+        data = discovery_service.contributor_search(url_connect, "persistence-layer")
+
     # take the JSON string and turn is into a Python object, the resulting object should be a list of dictionaries
     # Extracting content and links for pagination
         output_data = json.loads(data)
