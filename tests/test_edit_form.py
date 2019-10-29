c2fc1c65bc85655d4a20fd1549eea57bf6772e6b
David Morgan <github@ghost.me.uk>
2019-10-29 16:02:57 +0000

    Add test for new helper function

---
+++ tests/test_edit_form.py
@@ -0,0 +1,7 @@
+from app.forms.edit_form import extract_responses
+
+def test__extract_responses__valid_input__valid_output():
+    input_data = {'q900':'123', 'q901':'456', 'action':'saveForm'}
+    expected = [{'question':'q900', 'response':'123'}, {'question':'q901', 'response':'456'}]
+    output = extract_responses(input_data)
+    assert expected == output
