8a7bf439bc87c4e3ce8b83f46ab171fd5b37d6a4
ranjit-das <Ranjit.Das@ons.gov.uk>
2020-02-07 12:21:41 +0000

    LU-5215: selenium test added for load testing

---
+++ tests/load_test/config_test.py
@@ -0,0 +1,9 @@
+# Config file to set up UI_URL, REFERENCE
+# It assumes that chromedriver is already installed in $HOMEDIR/chromedriver
+# After setting the variables run concurrent_test.sh
+
+import os.path
+HOMEDIR = os.path.expanduser("~")
+CHROME_DRIVER_LOCATION = HOMEDIR + '/chromedriver'
+UI_URL = 'http://a441d17a3499511ea9c2e0a5091216e0-e21bbc0eb23ae5b5.elb.eu-west-2.amazonaws.com:5001'
+REFERENCE = '10000000002'
