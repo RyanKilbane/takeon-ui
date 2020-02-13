8777d17a2f7ddc56695aafa0f448372b81fe6d02
ranjit-das <Ranjit.Das@ons.gov.uk>
2020-02-13 14:06:02 +0000

    LU-5407: selenium load test moved so that it does not run as a part of other tests(pytest)

--- load_test/config_test.py
+++ load_test/config_test.py
@@ -6,4 +6,4 @@ import os.path
 HOMEDIR = os.path.expanduser("~")
 CHROME_DRIVER_LOCATION = HOMEDIR + '/chromedriver'
 UI_URL = 'http://a441d17a3499511ea9c2e0a5091216e0-e21bbc0eb23ae5b5.elb.eu-west-2.amazonaws.com:5001'
-REFERENCE = '10000000002'
+REFERENCE = '49900002387'
