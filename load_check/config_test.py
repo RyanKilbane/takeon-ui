f498e55882b17ed748bea50e73188b59791a1fed
mattprice-28 <mattprice_28@hotmail.co.uk>
2020-02-14 14:38:39 +0000

    resolving merge conflict

--- load_check/config_test.py
+++ load_check/config_test.py
@@ -6,8 +6,4 @@ import os.path
 HOMEDIR = os.path.expanduser("~")
 CHROME_DRIVER_LOCATION = HOMEDIR + '/chromedriver'
 UI_URL = 'http://a441d17a3499511ea9c2e0a5091216e0-e21bbc0eb23ae5b5.elb.eu-west-2.amazonaws.com:5001'
-<<<<<<< HEAD:tests/load_test/config_test.py
-REFERENCE = '10000000002'
-=======
 REFERENCE = '49900002387'
->>>>>>> 8777d17a2f7ddc56695aafa0f448372b81fe6d02:load_test/config_test.py
