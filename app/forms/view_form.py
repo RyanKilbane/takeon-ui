1696e8c0dd4d9434cf96f0aeb353f77a52a4f228
mattprice-28 <mattprice_28@hotmail.co.uk>
2020-03-24 07:53:07 +0000

    Combine save & validate

--- app/forms/view_form.py
+++ app/forms/view_form.py
@@ -41,34 +41,8 @@ def view_form(inqcode, period, ruref):
         zip(["survey", "period", "reference"], [inqcode, period, ruref]))
     parameters = build_uri(url_parameters)
 
-    if request.form and request.form['action'] == 'saveForm':
-        try:
-            response_data = extract_responses(request.form)
-            log.info('Response data: %s', response_data)
-            # Build up JSON structure to save
-            json_output = {}
-            json_output["responses"] = response_data
-            json_output["user"] = get_user()
-            json_output["reference"] = ruref
-            json_output["period"] = period
-            json_output["survey"] = inqcode
-
-            # Send the data to the business layer for processing
-            log.info("Output JSON: %s", str(json_output))
-            api_caller.save_response(parameters=parameters, data=json_output)
-            status_message = 'Responses saved successfully'
-        except HTTPError as http_error:
-            status_message = 'There was a problem with the HTTP call ' + http_error
-            log.info('HTTP Error %s', http_error)
-        except ConnectionError as connection_error:
-            status_message = 'There was a problem with the connection ' + connection_error
-            log.info('Connection Error %s', connection_error)
-        except Timeout as timeout_error:
-            status_message = 'Timeout error ' + timeout_error
-            log.info('Timeout Error %s', timeout_error)
-        except RequestException as requests_error:
-            status_message = 'There was a problem with your request ' + requests_error + 'Please contact Take-On Support Team'
-            log.info('Requests Error: %s', requests_error)
+    # if request.form and request.form['action'] == 'saveForm':
+    #     save_form(parameters, request.form, inqcode, period, ruref)
 
     contributor_details = api_caller.contributor_search(parameters=parameters)
     validation_outputs = api_caller.validation_outputs(parameters=parameters)
@@ -92,42 +66,9 @@ def view_form(inqcode, period, ruref):
              filter_validations(validations))
     log.info("Combined Response and Validation Info Data: %s", response_and_validations)
 
-
-    # validate button logic
-    if request.method == "POST" and request.form['action'] == "validate":
-        log.info('save validation button pressed')
-        json_data = {"survey": inqcode, "period": period,
-                     "reference": ruref, "bpmId": "0"}
-        header = {"x-api-key": api_key}
-        status_message = 'Validation Run Successfully'
-        try:
-            response = api_caller.run_validation(
-                url, json.dumps(json_data), header)
-            log.info("Response from SQS: %s", response)
-        except HTTPError as http_err:
-            status_message = "Http Error. Unable to call URL"
-            log.info('URL error occurred: %s', http_err)
-        except ConnectionError as connection_err:
-            status_message = "Connection Error. Unable to Connect to API Gateway"
-            log.info('API request error occured: %s', connection_err)
-        except Exception as e:
-            status_message = 'Validation Error. Kubernetes secret does not exist or is incorrect'
-            log.info('Validation Error Occurred: %s', e)
-            return render_template(
-                template_name_or_list="./error_templates/validate_error.html",
-                error=e
-            )
-        return render_template(
-            template_name_or_list=form_view_template_HTML,
-            survey=inqcode,
-            period=period,
-            ruref=ruref,
-            data=response_and_validations,
-            override_button=override_button,
-            status_message=json.dumps(status_message),
-            contributor_details=contributor_data['data'][0],
-            validation=filter_validations(validations),
-            status_colour=status_colour)
+    if request.form and request.form['action'] == 'saveForm':
+        save_form(parameters, request.form, inqcode, period, ruref)
+        validate(inqcode, period, ruref, response_and_validations, override_button, contributor_data, validations, status_colour)
 
     return render_template(
         template_name_or_list=form_view_template_HTML,
@@ -171,3 +112,68 @@ def override_all_button(data):
         if validation_triggered_counter == 2:
             return True
     return False
+
+def save_form(parameters, requestform, inqcode, period, ruref):
+    try:
+        response_data = extract_responses(requestform)
+        log.info('Response data: %s', response_data)
+        # Build up JSON structure to save
+        json_output = {}
+        json_output["responses"] = response_data
+        json_output["user"] = get_user()
+        json_output["reference"] = ruref
+        json_output["period"] = period
+        json_output["survey"] = inqcode
+
+        # Send the data to the business layer for processing
+        log.info("Output JSON: %s", str(json_output))
+        api_caller.save_response(parameters=parameters, data=json_output)
+        status_message = 'Responses saved successfully'
+        return status_message
+    except HTTPError as http_error:
+        status_message = 'There was a problem with the HTTP call ' + http_error
+        log.info('HTTP Error %s', http_error)
+    except ConnectionError as connection_error:
+        status_message = 'There was a problem with the connection ' + connection_error
+        log.info('Connection Error %s', connection_error)
+    except Timeout as timeout_error:
+        status_message = 'Timeout error ' + timeout_error
+        log.info('Timeout Error %s', timeout_error)
+    except RequestException as requests_error:
+        status_message = 'There was a problem with your request ' + requests_error + 'Please contact Take-On Support Team'
+        log.info('Requests Error: %s', requests_error)
+
+def validate(inqcode, period, ruref, response_and_validations, override_button, contributor_data, validations, status_colour):
+    log.info('save validation button pressed')
+    json_data = {"survey": inqcode, "period": period,
+                 "reference": ruref, "bpmId": "0"}
+    header = {"x-api-key": api_key}
+    status_message = 'Validation Run Successfully'
+    try:
+        response = api_caller.run_validation(
+            url, json.dumps(json_data), header)
+        log.info("Response from SQS: %s", response)
+    except HTTPError as http_err:
+        status_message = "Http Error. Unable to call URL"
+        log.info('URL error occurred: %s', http_err)
+    except ConnectionError as connection_err:
+        status_message = "Connection Error. Unable to Connect to API Gateway"
+        log.info('API request error occured: %s', connection_err)
+    except Exception as e:
+        status_message = 'Validation Error. Kubernetes secret does not exist or is incorrect'
+        log.info('Validation Error Occurred: %s', e)
+        return render_template(
+            template_name_or_list="./error_templates/validate_error.html",
+            error=e
+        )
+    return render_template(
+        template_name_or_list=form_view_template_HTML,
+        survey=inqcode,
+        period=period,
+        ruref=ruref,
+        data=response_and_validations,
+        override_button=override_button,
+        status_message=json.dumps(status_message),
+        contributor_details=contributor_data['data'][0],
+        validation=filter_validations(validations),
+        status_colour=status_colour)
